e2a46642b5608173b79d6df4ccf7e73b538ed5e1
School Practical Project <coclover9878@gmail.com>
2023-12-27 14:06:40 +0530

    all file
    add header animation

--- Animeted WebSite/code/script.js
+++ Animeted WebSite/code/script.js
@@ -1,37 +1,41 @@
+locoMotiveScroller()
 
-gsap.registerPlugin(ScrollTrigger);
-
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
-const locoScroll = new LocomotiveScroll({
-    el: document.querySelector("#main"),
-    smooth: true
-});
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
-
-// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy("#main", {
-    scrollTop(value) {
-        return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-    getBoundingClientRect() {
-        return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-    },
-    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-    pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-});
+function locoMotiveScroller() {
 
 
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+        pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
 
 
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
 
 
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+
+}
 
 
 
@@ -43,7 +47,86 @@ function onload() {
     cursorAnimation();
     page2Animation();
     page3Animation();
+    headerAnimation()
 }
+function headerAnimation() {
+    let openBtn = document.querySelector('#open-menu-btn');
+    let closeBtn = document.querySelector('#close-menu-btn');
+    let menuContainer = document.querySelector('.nav-container')
+    openBtn.addEventListener('click', () => {
+        gsap.to('.nav-container', {
+            y: { value: 0 },
+            x: 0,
+            duration: 1,
+            stagger: 0.5,
+            opacity: 1,
+        });
+        openBtn.style.display = 'none';
+        closeBtn.style.display = 'block';
+
+        gsap.to('.left-nav video', {
+            scale: 1,
+            opacity: 1,
+            duration: 1
+        });
+        gsap.to('.left-nav .video-con span', {
+            y: 20,
+            scale: 1,
+            opacity: 1,
+            duration: 1,
+            stagger: 0.3
+        });
+
+        gsap.to('.right-nav ul li', {
+            scale: 1,
+            opacity: 1,
+            duration: 1,
+            stagger: 0.1,
+        });
+        gsap.to('.right-nav ul button', {
+            scale: 1,
+            opacity: 1,
+            delay: 0.5,
+            duration: 1,
+            stagger: 0.1,
+        });
+
+    })
+    closeBtn.addEventListener('click', () => {
+        gsap.to(menuContainer, {
+            opacity: 0,
+            x: -500,
+            duration: 1,
+        });
+
+        openBtn.style.display = 'block';
+        closeBtn.style.display = 'none';
+
+        gsap.to('.left-nav video', {
+            scale: 0,
+            opacity: 0,
+            duration: 1
+        });
+
+        gsap.to('.left-nav .video-con span', {
+            y: 0,
+            scale: 0,
+            opacity: 0,
+            duration: 1,
+            stagger: 0.3
+        });
+        gsap.to('.right-nav ul li', {
+            scale: 0,
+            opacity: 0,
+            stagger: 0.1,
+            duration: 1,
+        });
+    });
+}
+
+
+
+
 
 
 function cursorAnimation() {
@@ -69,6 +152,30 @@ function cursorAnimation() {
 
         });
     });
+
+    let tl = gsap.timeline()
+
+
+    tl.from("#page1-content nav h3,#page1-content nav h4", {
+        y: -100,
+        duration: 1,
+        opacity: 0,
+        // delay: 1,
+        stagger: 0.4
+    });
+    tl.from("#page1-content  h1 span", {
+        y: 100,
+        scale: 1,
+        duration: 0.5,
+        opacity: 0,
+        stagger: 0.2
+    });
+    // tl.from("#page1-content  h1", {
+    //     y: 200,
+    //     duration: 0.5,
+    //     // opacity: 0,
+    //     stagger: 0.2
+    // });
 
 }
 
