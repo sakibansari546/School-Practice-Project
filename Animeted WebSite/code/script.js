7ede2dc715bd9e01bd64654d793cbb007ab556c0
School Practical Project <152527120+sakibansari546@users.noreply.github.com>
2023-12-27 09:47:32 +0530

    code/index.html
    add js code

--- Animeted WebSite/code/script.js
+++ Animeted WebSite/code/script.js
@@ -1,4 +1,38 @@
 
+gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+        return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+        return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+
+
 
 
 
@@ -8,6 +42,7 @@ onload()
 function onload() {
     cursorAnimation();
     page2Animation();
+    page3Animation();
 }
 
 
@@ -38,24 +73,84 @@ function cursorAnimation() {
 }
 
 function page2Animation() {
-    gsap.to("#page2Span", {
-        y: -12,
+
+    gsap.from("#page2 h1", {
+        y: 120,
+        stagger: 0.25,
+        duration: 1,
         stagger: 0.2,
         duration: 1,
         scrollTrigger: {
-            trigger: " #page2Span",
-            scroller: "body",
-            start: "top 47%",
+            trigger: " #page2",
+            scroller: "#main",
+            start: "top 40%",
             end: "top 46%",
-            markers: true,
-            scrub: 2
+            // markers: true,
+            scrub: 5
         }
-    })
+    });
+
+    gsap.from(".elm-left h4", {
+        x: -120,
+        stagger: 0.25,
+        duration: 1,
+        stagger: 0.2,
+        duration: 1,
+        scrollTrigger: {
+            trigger: " #page2",
+            scroller: "#main",
+            start: "top 60%",
+            end: "top 46%",
+            // markers: true,
+            scrub: 5
+        }
+    });
+    gsap.from(".elm-right h4", {
+        x: 120,
+        stagger: 0.25,
+        duration: 1,
+        stagger: 0.2,
+        duration: 1,
+        scrollTrigger: {
+            trigger: " #page2",
+            scroller: "#main",
+            start: "top 60%",
+            end: "top 46%",
+            // markers: true,
+            scrub: 5
+        }
+    });
 }
-// gsap.to("", {
-//     y: -12,
-//     scrollTrigger: {
-//         trigger: "#page2 #page2Span",
-//         scroller: "body"
-//     }
-// })
+
+function page3Animation() {
+    gsap.from("#page3 h2", {
+        y: 120,
+        stagger: 0.25,
+        duration: 1,
+        stagger: 0.2,
+        duration: 1,
+        scrollTrigger: {
+            trigger: " #page3",
+            scroller: "#main",
+            start: "top 70%",
+            end: "top 70%",
+            // markers: true,
+            scrub: 5
+        }
+    });
+    gsap.from("#page3 h4", {
+        x: 120,
+        stagger: 0.25,
+        duration: 1,
+        stagger: 0.2,
+        duration: 1,
+        scrollTrigger: {
+            trigger: " #page3",
+            scroller: "#main",
+            start: "top 75%",
+            end: "top 75%",
+            // markers: true,
+            scrub: 5
+        }
+    });
+}
